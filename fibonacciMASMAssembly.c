f4dba0bdc1600ebf74593e221bdf2f9375214c5d
gabriel88766 <56970226+gabriel88766@users.noreply.github.com>
2020-08-04 04:22:46 -0300

    Update fibonacciMASMAssembly.c
    Optimization: no memory access in function

--- fibonacciMASMAssembly.c
+++ fibonacciMASMAssembly.c
@@ -2,6 +2,11 @@
  If you want to run this code you need to compile as below
  gcc -m32 -masm=intel fibonacciMASMAssembly.c
 
+It only will work if n is stored in 
+
+Input : n - number you want to see output from fibonacci sequence
+Output : Fn, where F0 = 1, F1 = 1 , Fn = Fn-1 + Fn-2
+
 */
 #include <stdio.h>
 
@@ -10,36 +15,36 @@ __asm(
     "fibonacci:\n"
     "push ebp\n"
     "mov ebp, esp\n"
-    "sub esp, 12\n"
-    "mov DWORD PTR[ebp-8], 1\n" /*F_n*/
-    "mov DWORD PTR[ebp-4], 1\n" /*b, F_(n+1)*/
-    "mov DWORD PTR[ebp-12], 0\n"
+    "mov eax, 1\n" /*Fn*/
+    "mov ebx, 1\n" /*b, Fn+1*/
+    "mov esi, 0\n" /*for(i=0) */
+    "mov edx, DWORD PTR[esp+8]\n" /*n */
     "beginfor:\n"
-    "mov eax, DWORD PTR[ebp+8]\n" /*next three lines, if(i>=n) then jump to endfor */
-    "cmp DWORD PTR[ebp-12], eax\n" 
+    "cmp esi, edx\n" /*next two lines, if(i>=n) then jump to endfor */
     "jge endfor\n"
-    "mov eax, DWORD PTR[ebp-4]\n" /*old b value*/
-    "mov edx, DWORD PTR[ebp-8]\n"
-    "add DWORD PTR[ebp-4], edx\n" /*update b*/
-    "mov DWORD PTR[ebp-8], eax\n" /*update a */
-    "add DWORD PTR[ebp-12], 1\n" /* i++ */
+    "mov edi, ebx\n" /*old Fn+1 value stored in edi*/
+    "add ebx, eax\n" /*update Fn+1*/
+    "mov eax, edi\n" /*update Fn = old Fn+1 */
+    "add esi, 1\n" /* i++ */
     "jmp beginfor\n" 
     "endfor:\n"
-    "mov eax, DWORD PTR[ebp-8]\n"
-    "mov esp, ebp\n"
-    "pop ebp\n"
+    "pop ebp\n" 
     "ret 4"
 );
 
+
+
 int main(){
-    int a=2,n=10,S=0; /*You need to choose n*/
+    int n;
+    scanf("%d",&n);
     __asm(
-    "mov eax, DWORD PTR[ebp-16]\n" /*this code only works if &n = ebp-16 and &S = ebp-20 */
-    "push eax\n"
-    "call fibonacci\n"
-    "mov DWORD PTR[ebp-20], eax"
-    );
-    printf("%d",S);
+	"push ebx\n"
+	"call fibonacci"
+	:"=a" (n) /*output is n and comes from eax*/
+	:"b" (n)  /* input is n and is in ebx */
+	);
+    printf("\n\nFn = %d",n);
+    
     return 0;
     
 }
