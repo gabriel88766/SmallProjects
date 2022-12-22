ba0f1348c5fcd99a0a4c4b2a3b35d35887a4a4d1
gabriel88766 <gabriel.souzasilva125@gmail.com>
2022-12-22 10:58:31 -0300

    bash-loops

---
+++ Bash/Loops/echo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#echoes numbers from 1 to 10
+#while
+number=1
+while [ $number -le 10 ]
+do
+    echo $number
+    number=$(($number+1)) #another way, number=`expr $number+1`
+done
+
---
+++ Bash/Loops/fibonacci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#execute ./fibonacci.sh number
+#check if there is args and exit with error
+if [ $# -lt 1 ]
+then 
+    1>&2 echo "You must pass 1 positive number as argument"
+    exit 1
+fi
+
+if [ $1 -eq 1 -o $1 -eq 2 ]
+then
+    echo "1"
+fi
+
+a=1
+b=1
+c=2
+count=2
+while [ $count -lt $1 ] 
+do
+    count=`expr $count + 1`;
+    #c=`expr $a + $b`
+    c=$((a + b))
+    a=$b
+    b=$c 
+done
+echo $c
