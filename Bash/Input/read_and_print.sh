4b86158244e0de126b8bda61401e2a494ec2da62
gabriel88766 <gabriel.souzasilva125@gmail.com>
2022-12-23 11:56:28 -0300

    some bashes

---
+++ Bash/Input/read_and_print.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#How to use = ./read_and_print.sh file
+# < file pass file as input to a command
+# > write in file
+if [ $# -ne 1 ]
+then
+    2>&1 echo "too few or too many args. expected 1, have $#"
+    exit 0
+fi
+string_test=$(< $1) #reading and storing file in string
+#checking if there are differences
+xxd <<< $string_test > file1.ignore
+xxd $1 > file2.ignore
+if [[  file1 == file2 ]]
+then
+    echo "its equal"
+else
+    echo "its not equal"
+fi
+
+echo "$string_test" #right way to print a string
+echo $string_test > file3.ignore #ignoring 0x0a of $string_test
+
--- Bash/Loops/fibonacci.sh
+++ Bash/Loops/fibonacci.sh
@@ -20,7 +20,18 @@ count=2
 while [ $count -lt $1 ] 
 do
     count=`expr $count + 1`;
-    #c=`expr $a + $b`
+    c=`expr $a + $b`
+    a=$b
+    b=$c 
+done
+echo $c
+
+a=1
+b=1
+c=2
+for i in $(seq 3 $1)
+do
+    count=$((count + 1))
     c=$((a + b))
     a=$b
     b=$c 
